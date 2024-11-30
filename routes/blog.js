2b81c65a72db77f3779767218712b87939cc1633
Konrad <gimmy6707@gmail.com>
2024-11-30 13:54:03 +0900

    Now post-item in post-list shows up. Yet, those buttons on the item doesn't work yet.

--- routes/blog.js
+++ routes/blog.js
@@ -5,11 +5,17 @@ const db = require('../data/database')
 const router = express.Router();
 
 router.get('/', function(req,res){
+    
     res.redirect('/posts');
 }); 
 
-router.get('/posts', function(req,res){
-    res.render('posts-list');
+router.get('/posts', async function(req,res){
+    const query = `
+        SELECT POSTS.*, AUTHORS.NAME AS AUTHOR_NAME FROM POSTS 
+        INNER JOIN AUTHORS ON POSTS.AUTHOR_ID = AUTHORS.ID
+    `;
+    const [posts] = await db.query(query);
+    res.render('posts-list', {posts: posts});
 });
 
 router.get('/new-post', async function(req, res){
